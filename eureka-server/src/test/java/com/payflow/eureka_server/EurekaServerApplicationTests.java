0b649f86fd75a57133dc8c10eea416c2f68b04da
Vaishali-962 <vaishaliwankhade69200@gmail.com>
2026-04-10 15:05:39 +0530

    chore: initial monorepo setup with eureka, gateway and auth service

---
+++ eureka-server/src/test/java/com/payflow/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payflow.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
